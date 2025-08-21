7205f4099d8d280648228e2a9aa9660b1b18c077
Krishna1997-coder <pillutlamurali12@gmail.com>
2025-08-21 22:35:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar.jsx'
-import TopicForm from './components/TopicForm.jsx'
-import StudyLogForm from './components/StudyLogForm.jsx'
-import MockTestForm from './components/MockTestForm.jsx'
-import ProgressDashboard from './components/ProgressDashboard.jsx'
+import Navbar from './Components/Navbar.jsx'
+import TopicForm from './Components/TopicForm.jsx'
+import StudyLogForm from './Components/StudyLogForm.jsx'
+import MockTestForm from './Components/MockTestForm.jsx'
+import ProgressDashboard from './Components/ProgressDashboard.jsx'
 
 export default function App() {
   const [activeTab, setActiveTab] = useState("dashboard")
